c4f18c6c9db1aeda95b55435eab6a672820a7ef8
Tyler Hemingsen <hemingsent@gmail.com>
2022-07-24 13:56:31 -0700

    create manifest

---
+++ LearningModuleTemplate/LearningModuleManifest.ps1
@@ -0,0 +1,7 @@
+$manifestProperties = @{
+    Path = ".\LearningModuleTemplate\PlasterManifest.xml"
+    Title = "Learning Module Template"
+    TemplateName = 'LearningModuleTemplate'
+    TemplateVersion = '0.0.1'
+    Author = 'Tyler Hemingsen'
+}
